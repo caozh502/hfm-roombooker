59e159281baf5790ccd693efd4b4b997a223c0c7
Caleb <328776293@qq.com>
2022-04-05 19:27:39 +0200

    login finished

--- test.py
+++ test.py
@@ -5,14 +5,17 @@ url = 'https://hfm-karlsruhe.asimut.net/public/login.php'
 header = { 'User-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36'
 }
 
-from_data = {'username': '13200',
-    'password': 'ZXY200238zxy.',
-    'code': '',
-    'lt': 'xxx',
-    'execution': 'e1s1',
-    '_eventId': 'submit',
-    'submit': '登录'
+from_data = {'authenticate-useraccount': '13200',
+    'authenticate-password': 'ZXY200238zxy.',
+    'authenticate-url': '/public/',
+    'authenticate-verification': 'ok',
+    # 'execution': 'e1s1',
+    # '_eventId': 'submit',
+    # 'submit': '登录'
 }
 s = requests.session()
 response = s.post(url, data = from_data, headers = header)
-print response.status_code
+if 'Xinyuan Zhang' in response.content.decode('utf-8'):
+    print ('successfully login')
+else:
+    print ('failed login')
